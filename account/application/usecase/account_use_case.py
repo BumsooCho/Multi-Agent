7c819dc15372daed9022de51441f3ab7e846b398
DESKTOP-J3SCFF3\Ksmartech <bscho@ksmartech.com>
2025-11-19 19:17:22 +0900

    feat : [조범수] account 싱글톤 패턴 구조 적용 [AIS-3]

--- account/application/usecase/account_use_case.py
+++ account/application/usecase/account_use_case.py
@@ -1,8 +1,23 @@
+from account.infrastructure.repository.account_repository_impl import AccountRepositoryImpl
 from account.infrastructure.service.account_use_service import AccountUseService
+from account.domain.account import Account
 
 class AccountUseCase:
-  def __init__(self, service: AccountUseService):
-    self.service = service
   
+  __instance = None
+  
+  def __new__(cls, *args, **kwargs):
+    if cls.__instance is None:
+      cls.__instance = super().__new__(cls)
+      cls.__instance.account_repo = AccountRepositoryImpl.getInstance()
+    return cls.__instance
+  
+  @classmethod
+  def getInstance(cls):
+    if cls.__instance is None:
+      cls.__instance = cls()
+    return cls.__instance
+      
   def create_or_get_account(self, email, name):
-    return self.service.create_or_get_account(self, email, name)
+    account = Account(email=email, name=name)
+    return self.account_repo.create_or_get_account(account)
--- account/infrastructure/repository/account_repository_impl.py
+++ account/infrastructure/repository/account_repository_impl.py
@@ -7,16 +7,44 @@ from sqlalchemy.orm import Session
 from config.database.session import get_db_session
 
 class AccountRepositoryImpl(AccountRepositoryPort):
-  def __init__(self):
-    self.db: Session = get_db_session()
   
-  def create_or_get_account(self, email, name) -> Account:
+  __instance = None
+  
+  def __new__(cls, *args, **kwargs):
+      if cls.__instance is None:
+          cls.__instance = super().__new__(cls)
+      return cls.__instance
+  
+  @classmethod
+  def getInstance(cls):
+      if cls.__instance is None:
+          cls.__instance = cls()
+      return cls.__instance
+  
+  def __init__(self):
+      if not hasattr(self, 'db'):
+          self.db: Session = get_db_session()
+  
+  def save(self, account: Account) -> Account:
+    orm_account = AccountORM(
+      email = account.email,
+      name = account.name
+    )
+    self.db.add(orm_account)
+    self.db.commit()
+    self.db.refresh(orm_account)
+    
+    return account
+    
+            
+  def create_or_get_account(self, account: Account) -> Account:
     orm_account = self.db.query(AccountORM).filter(AccountORM.email == email).first()
     if orm_account:
       account = Account(
         email = orm_account.email,
         name = orm_account.name,
       )
-      return account
-    return None
+    else:
+      self.save(account)
+    return account
       
